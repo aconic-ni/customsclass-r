7a8fbb8b2bdfdb436fd26df34685cadd4fbd6162
Jordy St Juárez <jordystjuarez@gmail.com>
2025-07-25 22:41:51 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,8 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,11 +12,15 @@ const firebaseConfig = {
   projectId: "customsclass-r-6a2cf",
   storageBucket: "customsclass-r-6a2cf.firebasestorage.app",
   messagingSenderId: "430062147128",
-  appId: "1:430062147128:web:5a91810cb1f2001432b8cd",
-  measurementId: "G-5JC10J47GX"
+  appId: "1:430062147128:web:005c9a19a919f85932b8cd",
+  measurementId: "G-SBN0L7M87X"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Initialize Analytics and export it
+// We are checking if analytics is supported by the browser
+export const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
